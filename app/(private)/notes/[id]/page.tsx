5cfb3b91dc83781c115acd1ad62f5234999ede93
denis-mahei <denmahei@gmail.com>
2026-07-20 22:02:01 +0300

    add og meta for note id

--- app/(private)/notes/[id]/page.tsx
+++ app/(private)/notes/[id]/page.tsx
@@ -7,6 +7,30 @@ interface PageProps {
   params: Promise<{ id: string }>;
 }
 
+export async function generateMetadata({ params }: PageProps) {
+  const { id } = await params;
+  const note = await getNoteById(id);
+  return {
+    title: `Note: ${note.title}`,
+    description: note.content.slice(0, 30),
+    openGraph: {
+      title: `Note: ${note.title}`,
+      description: note.content.slice(0, 50),
+      url: `https://notehub.com/notes/${id}`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: note.title,
+        },
+      ],
+      type: 'article',
+    },
+  };
+}
+
 const Page = async ({ params }: PageProps) => {
   const { id } = await params;
   const note = await getNoteById(id);
